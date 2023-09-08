b0b8017b7d960118d4ce87d74cffb3c184b9957f
mdiktushar <md.tushar1804@gmail.com>
2023-09-08 14:56:06 +0600

    3 complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/admin', [AdminsController::class, 'index'])->name('admin.index');
