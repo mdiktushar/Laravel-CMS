12e055bc88c63444db4af2d7a050ccc07f6292ec
mdiktushar <md.tushar1804@gmail.com>
2023-09-09 13:45:03 +0600

    complete 4

--- resources/views/components/home-master.blade.php
+++ resources/views/components/home-master.blade.php
@@ -36,6 +36,17 @@
               <span class="sr-only">(current)</span>
             </a>
           </li>
+          @if (Auth::check())
+            <li class="nav-item">
+              <a class="nav-link" href={{'/admin'}}>Admin</a>
+            </li>
+          @else
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+            </li>          
+          @endif
+          
+          
           <li class="nav-item">
             <a class="nav-link" href="#">About</a>
           </li>
