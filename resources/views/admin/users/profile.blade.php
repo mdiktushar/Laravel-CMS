8d1c0cca3e28a70f6e62fd77a9fc5caa1bdec0c2
mdiktushar <md.tushar1804@gmail.com>
2023-09-17 17:43:39 +0600

    32.2

--- resources/views/admin/users/profile.blade.php
+++ resources/views/admin/users/profile.blade.php
@@ -1,5 +1,40 @@
 <x-admin-master>
     @section('content')
-        <h1>User Profile for : {{$user->name}}</h1>
+        <h1>User Profile for : {{ $user->name }}</h1>
+
+        <div class="row">
+            <div class="col-sm-6">
+                <form>
+                    <div class="mb-4">
+                        <img width="50px" class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+                    </div>
+                    <div class="form-group">    
+                        <input type="file" name="" id="">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" name="name" value={{ $user->name }}>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" class="form-control" name="email" value={{ $user->email }}>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Confirm Password</label>
+                        <input type="password" class="form-control" name="password_confirmation">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
     @endsection
 </x-admin-master>
