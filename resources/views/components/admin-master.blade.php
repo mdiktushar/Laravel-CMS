be651c1cf945c7560427d1613f65db68d081382f
mdiktushar <md.tushar1804@gmail.com>
2023-09-18 17:59:40 +0600

    33.9

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -55,9 +55,9 @@
       </div>
 
       <!-- Nav Item - Pages Collapse Menu -->
-      <x-admin-sidebar-posts-link />
+      <x-admin.sidebar.admin-sidebar-posts-link />
       @if (auth()->user()->userHasRole('Admin'))
-        <x-admin-sidebar-users-link />
+        <x-admin.sidebar.admin-sidebar-users-link />
       @endif
       
 
@@ -288,7 +288,7 @@
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
-            <x-admin-top-navbar-user-information></x-admin-top-navbar-user-information>
+            <x-admin.top-nav.admin-top-navbar-user-information></x-admin-top-navbar-user-information>
 
           </ul>
 
